62f7493510826553e440aadee7e7a3a8103b96cf
guguke <gbm618@gmail.com>
2013-08-07 18:00:52 -0400

    add

--- broadcast/linux/mrs.c
+++ broadcast/linux/mrs.c
@@ -1,8 +1,10 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <net/if.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>

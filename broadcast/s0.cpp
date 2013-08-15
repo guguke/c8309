2dcaf35be597aa5ae48b482b95b76e5c07c2ae53
gbm618 <gbm618@gmail.com>
2013-08-15 18:03:28 +0800

    vc

--- broadcast/s0.cpp
+++ broadcast/s0.cpp
@@ -2,11 +2,22 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
+#include <time.h>
 #define MC_ADDR     "226.1.1.1"
 #define MC_PORT     4321
 #define MAXLEN      256
 
 // vc6 compile : prj setting add ws2_32.lib
+
+
+void getTimeStr(char *str)
+{
+	time_t t;
+	tm *now;
+	time(&t);
+	now=localtime(&t);
+	sprintf(str,("%02d:%02d:%02d"),now->tm_hour,now->tm_min,now->tm_sec);
+}
 
 void main(int argc, char **argv)
 {
@@ -15,6 +26,7 @@ void main(int argc, char **argv)
 	int             nMcLen;
 	int             n;
 	char            buf[MAXLEN];
+	char sec[40];
 	WSADATA         wsaData;
 	int             nErr;    
 	if(WSAStartup(0x0202, &wsaData) != 0)
@@ -36,17 +48,19 @@ void main(int argc, char **argv)
 	n = 0;
 	//for(int i=0;i<3;i++)
 	//{
-		//Sleep(1000);
-		sprintf(buf, "getip 226.1.1.2 4322 ....");
-		nErr = sendto(s, buf, strlen(buf), 0, (struct sockaddr*)&mcAddr, nMcLen);
+	//Sleep(1000);
+	sec[0]=0;
+	getTimeStr(sec);
+	sprintf(buf, "getip 226.1.1.2 4322 %s",sec);
+	nErr = sendto(s, buf, strlen(buf), 0, (struct sockaddr*)&mcAddr, nMcLen);
 #if 0
-		if(nErr == SOCKET_ERROR)
-		{
-			//WSAReportErr("sendto(...)");
-			break;
-		}
+	if(nErr == SOCKET_ERROR)
+	{
+		//WSAReportErr("sendto(...)");
+		break;
+	}
 #endif
-		printf("sent: %s   (%s:%d\n", buf,MC_ADDR,MC_PORT);
+	printf("sent: %s   (%s:%d\n", buf,MC_ADDR,MC_PORT);
 	//}
 	WSACleanup();
 }

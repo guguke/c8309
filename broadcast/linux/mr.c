faa9e62171151041d1e982ec672bcc78fba39f35
gbm618 <gbm618@gmail.com>
2013-08-17 17:40:52 +0800

    argc

--- broadcast/linux/mr.c
+++ broadcast/linux/mr.c
@@ -1,11 +1,13 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <net/if.h>
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 // get if ip : if="eth0" or "eth2"      

3ec358620933489445a15637619119505f9a93bd
gbm618 <gbm618@gmail.com>
2013-09-30 16:09:58 +0800

    vc

--- m37/mongoose/main.c
+++ m37/mongoose/main.c
@@ -45,6 +45,7 @@
 #include <shlobj.h>
 #include <process.h>
 #include <time.h>
+//#include <strsafe.h>
 
 #define PATH_MAX MAX_PATH
 #define S_ISDIR(x) ((x) & _S_IFDIR)
@@ -292,6 +293,94 @@ void mylog(char *p)
 	fclose(fp);
 	return;
 }
+HANDLE g_hChildStd_IN_Rd = NULL;
+HANDLE g_hChildStd_IN_Wr = NULL;
+HANDLE g_hChildStd_OUT_Rd = NULL;
+HANDLE g_hChildStd_OUT_Wr = NULL;
+#if 0
+void ErrorExit(PTSTR lpszFunction) 
+
+// Format a readable error message, display a message box, 
+// and exit from the application.
+{ 
+    LPVOID lpMsgBuf;
+    LPVOID lpDisplayBuf;
+    DWORD dw = GetLastError(); 
+	
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        dw,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR) &lpMsgBuf,
+        0, NULL );
+	
+    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, 
+        (lstrlen((LPCTSTR)lpMsgBuf)+lstrlen((LPCTSTR)lpszFunction)+40)*sizeof(TCHAR)); 
+    StringCchPrintf((LPTSTR)lpDisplayBuf, 
+        LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+        TEXT("%s failed with error %d: %s"), 
+        lpszFunction, dw, lpMsgBuf); 
+    MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK); 
+	
+    LocalFree(lpMsgBuf);
+    LocalFree(lpDisplayBuf);
+    ExitProcess(1);
+}
+#endif
+void CreateChildProcess()
+// Create a child process that uses the previously created pipes for STDIN and STDOUT.
+{ 
+	TCHAR szCmdline[]=TEXT("plink -telnet -P 2000 192.168.1.77");///////////////////////////////////////////////// debug\\hello or child
+	PROCESS_INFORMATION piProcInfo; 
+	STARTUPINFO siStartInfo;
+	BOOL bSuccess = FALSE; 
+	int i;
+	
+	// Set up members of the PROCESS_INFORMATION structure. 
+	
+	ZeroMemory( &piProcInfo, sizeof(PROCESS_INFORMATION) );
+	
+	// Set up members of the STARTUPINFO structure. 
+	// This structure specifies the STDIN and STDOUT handles for redirection.
+	
+	ZeroMemory( &siStartInfo, sizeof(STARTUPINFO) );
+	siStartInfo.cb = sizeof(STARTUPINFO); 
+	siStartInfo.hStdError = g_hChildStd_OUT_Wr;
+	siStartInfo.hStdOutput = g_hChildStd_OUT_Wr;
+	siStartInfo.hStdInput = g_hChildStd_IN_Rd;
+	siStartInfo.dwFlags |= STARTF_USESTDHANDLES;
+	
+	// Create the child process. 
+    
+	bSuccess = CreateProcess(NULL, 
+		szCmdline,     // command line 
+		NULL,          // process security attributes 
+		NULL,          // primary thread security attributes 
+		TRUE,          // handles are inherited 
+		0,             // creation flags 
+		NULL,          // use parent's environment 
+		NULL,          // use parent's current directory 
+		&siStartInfo,  // STARTUPINFO pointer 
+		&piProcInfo);  // receives PROCESS_INFORMATION 
+	
+	// If an error occurs, exit the application. 
+	if ( ! bSuccess ) {
+		i=1;
+		//ErrorExit(TEXT("CreateProcess"));
+	}
+	else 
+	{
+		// Close handles to the child process and its primary thread.
+		// Some applications might keep these handles to monitor the status
+		// of the child process, for example. 
+		
+		CloseHandle(piProcInfo.hProcess);
+		CloseHandle(piProcInfo.hThread);
+	}
+}
 
 DWORD RunSilent(char* strFunct, char* strstrParams)
 {
@@ -306,7 +395,7 @@ DWORD RunSilent(char* strFunct, char* strstrParams)
 	memset(&StartupInfo, 0, sizeof(StartupInfo));
 	StartupInfo.cb = sizeof(STARTUPINFO);
 	StartupInfo.dwFlags = STARTF_USESHOWWINDOW;
-	StartupInfo.wShowWindow = SW_HIDE;
+	StartupInfo.wShowWindow = SW_HIDE;//////////////////////////// 
 	
 	Args[0] = 0;
 	

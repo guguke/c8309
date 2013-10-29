3efd4cdc69e3ca53fb45b7d3bb1f677a1ac847f3
guguke <gbm618@gmail.com>
2013-10-29 16:19:46 +0800

    argv

--- unixsocket/ur.c
+++ unixsocket/ur.c
@@ -2,16 +2,18 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <stdio.h>
-#define NAME "socket"
+//#define NAME "socket"
 /*
  * This program creates a UNIX domain datagram socket, binds a
  * name to it, then reads from the socket.
  */
-main()
+main(int argc,char *argv[])
 {
    int sock, length;
    struct sockaddr_un name;
    char buf[1024];
+   printf(" usage: ur name_unix_socket\n");
+   if(argc<2)return;
    /* Create socket from which to read. */
    sock = socket(AF_UNIX, SOCK_DGRAM, 0);
    if (sock < 0) {
@@ -21,7 +23,8 @@ main()
   
    /* Create name. */
    name.sun_family = AF_UNIX;
-   strcpy(name.sun_path, NAME);
+   //strcpy(name.sun_path, NAME);
+   strcpy(name.sun_path, argv[1]);
    //name.sun_len = strlen(name.sun_path);
    if (bind(sock, (struct sockaddr *)&name, SUN_LEN(&name))) {
       perror("binding name to datagram socket");
@@ -34,5 +37,6 @@ main()
       perror("receiving datagram packet");
    printf("-->%s\n", buf);
    close(sock);
-   unlink(NAME);
+   //unlink(NAME);
+   unlink(argv[1]);
 }

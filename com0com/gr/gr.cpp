1d095c4f1d9590763ec60a75dbf6d8e7181023d8
guguke <gbm618@gmail.com>
2014-05-16 16:10:11 +0800

    .net framework run not silent

--- com0com/gr/gr.cpp
+++ com0com/gr/gr.cpp
@@ -1,18 +1,30 @@
 // gr.cpp : main project file.
 
 #include "stdafx.h"
-#include "Form1.h"
+//#include "Form1.h"
 
-using namespace gr;
+//using namespace gr;
+using namespace System;
+using namespace System::IO;
+using namespace System::Diagnostics;
 
 [STAThreadAttribute]
 int main(array<System::String ^> ^args)
 {
 	// Enabling Windows XP visual effects before any controls are created
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false); 
+	//Application::EnableVisualStyles();
+	//Application::SetCompatibleTextRenderingDefault(false); 
 
 	// Create the main window and run it
-	Application::Run(gcnew Form1());
+	//Application::Run(gcnew Form1());
+	//return 0;
+
+	String ^cmdstr;
+	if(args->Length<1) return -1;
+	for(int i=1;i<args->Length;i++) cmdstr += args[i]+" ";
+
+	Process::Start(args[0],cmdstr);
+
 	return 0;
+
 }

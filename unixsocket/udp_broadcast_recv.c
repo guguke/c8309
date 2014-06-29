55532923f20b757b37c8edbd92a769b9a978d964
guguke <gbm618@gmail.com>
2014-06-29 15:38:03 +0800

    udp broadcast send/recv

--- unixsocket/udp_broadcast_recv.c
+++ unixsocket/udp_broadcast_recv.c
@@ -1,10 +1,10 @@
 /*
-        demo-udp-03: udp-recv: a simple udp server
-	receive udp messages
+demo-udp-03: udp-recv: a simple udp server
+receive udp messages
 
-        usage:  udp-recv
+usage:  udp-recv
 
-        Paul Krzyzanowski
+Paul Krzyzanowski
 */
 
 #include <stdlib.h>
@@ -15,61 +15,89 @@
 #include <arpa/inet.h>
 //#include "port.h"
 
-#define BUFSIZE 2048
+//#define BUFSIZE 2048
 
-int
-main(int argc, char **argv)
+//int main(int argc, char **argv)
+int udp_broadcast_recv(char *paddr,int nport,char buf,int bufsize)
 {
 	struct sockaddr_in myaddr;	/* our address */
 	struct sockaddr_in remaddr;	/* remote address */
 	socklen_t addrlen = sizeof(remaddr);		/* length of addresses */
 	int recvlen;			/* # bytes received */
 	int fd;				/* our socket */
-	unsigned char buf[BUFSIZE];	/* receive buffer */
-        int n;
-        int nport=9010;
-        int so_broadcast=1;
-
+	//unsigned char buf[BUFSIZE];	/* receive buffer */
+	int n;
+	int so_broadcast=1;
 
 	/* create a UDP socket */
-
 	if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		perror("cannot create socket\n");
-		return 0;
+		return -1;
 	}
 
 	/* bind the socket to any valid IP address and a specific port */
-
 	memset((char *)&myaddr, 0, sizeof(myaddr));
 	myaddr.sin_family = AF_INET;
 	//myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	if(inet_aton("127.255.255.255",&myaddr.sin_addr)==0){
-                fprintf(stderr,"inet_aton() failed\n");
-                exit(1);
-                close(fd);
-        }
+	if(inet_aton(paddr,&myaddr.sin_addr)==0){
+		printf("  inet_aton() failed\n");
+		close(fd);
+		return -2;
+	}
 	myaddr.sin_port = htons(nport);
 	//myaddr.sin_port = htons(SERVICE_PORT);
 
-        //if(-1 == setsockopt(fd,SOL_SOCKET,SO_BROADCAST,&so_broadcast,sizeof so_broadcast)) fprintf(stderr," setsockopt BROADCAST error\n");
-        // use SO_REUSEADDR not SO_REUSEPORT
-        if(-1 == setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&so_broadcast,sizeof so_broadcast)) fprintf(stderr," setsockopt REUSEADDR error\n");
+	//if(-1 == setsockopt(fd,SOL_SOCKET,SO_BROADCAST,&so_broadcast,sizeof so_broadcast)) fprintf(stderr," setsockopt BROADCAST error\n");
+	// use SO_REUSEADDR not SO_REUSEPORT
+	if(-1 == setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&so_broadcast,sizeof so_broadcast)) fprintf(stderr," setsockopt REUSEADDR error\n");
 
 	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
-		perror("bind failed");
-		return 0;
+		printf(" bind failed");
+		return -3;
 	}
 
-	/* now loop, receiving data and printing what we received */
-	for (n=0;n<5;n++) {
-		printf("waiting on port %d\n", nport);
-		recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
+	printf("waiting on port %d\n", nport);
+	for(;;){
+		recvlen = recvfrom(fd, buf, bufsize, 0, (struct sockaddr *)&remaddr, &addrlen);
 		printf("received %d bytes\n", recvlen);
 		if (recvlen > 0) {
-			buf[recvlen] = 0;
-			printf("received message(%d): \"%s\"\n", n, buf);
+			//buf[recvlen] = 0;
+			printf("received message: ");
+			for(n=0;n<recvlen;n++) printf(" %02x",0x0ff & buf[n]);
+			printf("\n");
+			break;
 		}
 	}
-        close(fd);
-	/* never exits */
+	close(fd);
+	return recvlen;
 }
+int main(int argc,char *argv[])
+{
+	char addr[100];
+	int nport;
+	char buf[2000];
+	int bufsize=1000;
+
+	strcpy(addr,"127.255.255.255");
+	nport=9010;
+
+	printf(" usage: udp_broadcast_recv ip_addr port \n");
+	printf("        udp_broadcast_recv \n");
+	printf("        udp_broadcast_recv 127.255.255.255 \n");
+	printf("        udp_broadcast_recv 127.255.255.255 9010 \n");
+
+	switch(argc){
+	case 3:
+		nport=atoi(argv[2]);
+	case 2:
+		strcpy(addr,argv[1]);
+	default:
+		break;
+	}
+	printf(" udp broadcast recv :   ip:%s  port:%d \n",addr,nport);
+
+	udp_broadcast_recv(addr,nport,buf,bufsize);
+	return 0;
+}
+
+
--- unixsocket/udp_broadcast_send.c
+++ unixsocket/udp_broadcast_send.c
@@ -1,12 +1,12 @@
 /*
-        demo-udp-03: udp-send: a simple udp client
-	send udp messages
-	sends a sequence of messages (the # of messages is defined in MSGS)
-	The messages are sent to a port defined in SERVICE_PORT
+demo-udp-03: udp-send: a simple udp client
+send udp messages
+sends a sequence of messages (the # of messages is defined in MSGS)
+The messages are sent to a port defined in SERVICE_PORT
 
-        usage:  udp-send
+usage:  udp-send
 
-        Paul Krzyzanowski
+Paul Krzyzanowski
 */
 
 #include <stdlib.h>
@@ -19,22 +19,24 @@
 #define BUFLEN 2048
 #define MSGS 10	/* number of messages to send */
 
-int main(void)
+//int main(void)
+// nn , send times
+int udp_broadcast_send(char *server,int nport,char *buf,int len,int nn)
 {
 	struct sockaddr_in myaddr, remaddr;
 	int fd, i, slen=sizeof(remaddr);
-	char *server = "127.255.255.255";	/* change this to use a different server */
-	char buf[BUFLEN];
-        int nport=9010;
-        int so_broadcast=1;
+	//char *server = "127.255.255.255";	/* change this to use a different server */
+	//char buf[BUFLEN];
+	//int nport=9010;
+	int so_broadcast=1;
 
 	/* create a socket */
-
-	if ((fd=socket(AF_INET, SOCK_DGRAM, 0))==-1)
+	if ((fd=socket(AF_INET, SOCK_DGRAM, 0))==-1){
 		printf("socket created\n");
+		return -3;
+	}
 
 	/* bind it to all local addresses and pick any port number */
-
 	memset((char *)&myaddr, 0, sizeof(myaddr));
 	myaddr.sin_family = AF_INET;
 	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -42,10 +44,11 @@ int main(void)
 
 	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
 		perror("bind failed");
-		return 0;
+		close(fd);
+		return -1;
 	}       
 
-        if(-1 == setsockopt(fd,SOL_SOCKET,SO_BROADCAST,&so_broadcast,sizeof so_broadcast)) fprintf(stderr," setsockopt BROADCAST error\n");
+	if(-1 == setsockopt(fd,SOL_SOCKET,SO_BROADCAST,&so_broadcast,sizeof so_broadcast)) fprintf(stderr," setsockopt BROADCAST error\n");
 	/* now define remaddr, the address to whom we want to send messages */
 	/* For convenience, the host address is expressed as a numeric IP address */
 	/* that we will convert to a binary format via inet_aton */
@@ -55,18 +58,56 @@ int main(void)
 	remaddr.sin_port = htons(nport);
 	if (inet_aton(server, &remaddr.sin_addr)==0) {
 		fprintf(stderr, "inet_aton() failed\n");
-		exit(1);
+		close(fd);
+		return -2;
 	}
-
-	/* now let's send the messages */
-
-	for (i=0; i < MSGS; i++) {
+	for(i=0;i<nn;i++){
 		printf("Sending packet %d to %s port %d\n", i, server, nport);
-		sprintf(buf, "This is packet %d", i);
-		if (sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&remaddr, slen)==-1)
-			perror("sendto");
-                sleep(10);
+		if (sendto(fd, buf, len, 0, (struct sockaddr *)&remaddr, slen)==-1){
+			printf("sendto error \n");
+			close(fd);
+			return -4;
+		}
+		if(nn>1) sleep(10);
 	}
 	close(fd);
 	return 0;
 }
+int main(int argc,char *argv[])
+{
+	char addr[100];
+	int nport;
+	char buf[2000];
+	int len;
+	int nn;
+
+	strcpy(addr,"127.255.255.255");
+	nport=9010;
+	strcpy(buf," msg udp broadcast send ");
+	len = strlen(buf);
+	nn = 1;
+
+	printf(" usage: udp_broadcast_send ip_addr port times msg_send \n"):
+	printf(" usage: udp_broadcast_send \n"):
+	printf(" usage: udp_broadcast_send 127.255.255.255 \n"):
+	printf(" usage: udp_broadcast_send 127.255.255.255 9010 \n"):
+	printf(" usage: udp_broadcast_send 127.255.255.255 9010 5 \n"):
+	printf(" usage: udp_broadcast_send 127.255.255.255 9010 5 msg.send.msg.send \n"):
+
+	switch(argc){
+	case 5:
+		strcpy(buf,argv[4]);
+		len=strlen(buf);
+	case 4:
+		nn=atoi(argv[3]);
+	case 3:
+		nport=atoi(argv[2]);
+	case 2:
+		strcpy(addr,argv[1]);
+	default:
+		break;
+	}
+	udp_broadcast_send(addr,nport,buf,len,nn);
+	return 0;
+}
+

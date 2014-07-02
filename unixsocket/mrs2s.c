4d75cd4cc5b9f8e81900cd7076d22e81306315a3
guguke <gbm618@gmail.com>
2014-07-02 16:54:32 +0800

    mrs2

--- unixsocket/mrs2s.c
+++ unixsocket/mrs2s.c
@@ -12,11 +12,87 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+/* listen on sock_fd, new connection on new_fd */
+int sockfd, new_fd;    // to global
+
 /* the port users will be connecting to */
 #define MYPORT 3490
 
 /* how many pending connections queue will hold */
 #define BACKLOG 10
+
+pthread_t tid[2];
+int ret1,ret2;
+
+void* waitUDP(void *arg)
+{
+	int len;
+	char buf[300];
+
+	unsigned long i = 0;
+	pthread_t id = pthread_self();
+
+	strcpy(buf,"alskjd asl das dfas df asdf asd fas  \n");
+	len = strlen(buf);
+	//len=read(new_fd,buf,200);
+	//if(len<0)continue;
+	//buf[len]=0;
+	//printf(" recv from client(%d) : %s \n",n1,buf);
+	for(i=0;i<3; i++){
+		if(send(new_fd, buf,len, 0) == -1){
+			perror("Server-send() error 1 lol!");
+			break;
+		}
+		sleep(4);
+	}
+#if 0
+	for(i=0; i<(0x0FFFF);i++);
+
+	if(pthread_equal(id,tid[0]))
+	{
+		printf("\n First thread processing done\n");
+		ret1  = 100;
+		//pthread_exit(&ret1);
+	}
+	else
+	{
+		printf("\n Second thread processing done\n");
+		ret2  = 200;
+		pthread_exit(&ret2);
+	}
+#endif
+	printf(" waitUDP == thread return null \n");
+	return NULL;
+}
+
+int startUDPrecv(void)
+{
+	int i = 0;  
+	int err;
+	int *ptr[2];
+
+	//while(i < 2)	{
+		err = pthread_create(&(tid[i]), NULL, &waitUDP, NULL);
+		if (err != 0)
+			printf("\ncan't create thread :[%s]", strerror(err));
+		else
+			printf("\n Thread created successfully\n");
+
+		//i++;
+	//}
+
+	pthread_join(tid[0], (void**)&(ptr[0]));
+	//pthread_join(tid[1], (void**)&(ptr[1]));
+
+	if( ptr[0] != NULL )printf("\n return value from first thread is [%d]\n", *ptr[0]);
+	//printf(" main:  thread exit return null \n");
+	//printf("\n return value from second thread is [%d]\n", *ptr[1]);
+
+	printf(" end of startUDPrecv \n");
+	return 0;
+}
+
+
 
 void sigchld_handler(int s)
 {
@@ -27,7 +103,7 @@ void sigchld_handler(int s)
 int main(int argc, char *argv[ ])
 {
 	/* listen on sock_fd, new connection on new_fd */
-	int sockfd, new_fd;
+	//int sockfd, new_fd;    // to global
 
 	/* my address information */
 	struct sockaddr_in my_addr;
@@ -99,6 +175,8 @@ int main(int argc, char *argv[ ])
 			/* child doesn¡¯t need the listener */
 			close(sockfd);
 
+			startUDPrecv();
+#if 0
 			for(n1=0;n1<3;n1++){
 				len=read(new_fd,buf,200);
 				if(len<0)continue;
@@ -108,6 +186,7 @@ int main(int argc, char *argv[ ])
 					perror("Server-send() error 1 lol!");
 					return -7;
 				}
+#endif
 			}
 			close(new_fd);
 			printf(" == fork end \n");

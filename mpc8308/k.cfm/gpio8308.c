925783961a76da507ab78a41731eb911013de94b
guguke <guguke@gmail.com>
2014-09-02 17:22:38 -0400

    ver c3f , add gpio proc

---
+++ mpc8308/k.cfm/gpio8308.c
@@ -0,0 +1,232 @@
+#include <linux/module.h> /* Specifically, a module */
+#include <linux/kernel.h> /* We're doing kernel work */
+#include <linux/proc_fs.h>        /* Necessary because we use the proc fs */
+#include <asm/uaccess.h>  /* for copy_from_user */
+
+#include <linux/stddef.h>
+
+#include <asm/io.h>
+#include <asm/hw_irq.h>
+#include <sysdev/fsl_soc.h>
+
+#include "mpc83xx.h"
+
+#if 0
+#include <linux/slab.h>
+#include <linux/types.h>
+#include <linux/delay.h>
+#include <linux/pm.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <linux/init.h>
+#include <linux/clk.h>
+#include <linux/platform_device.h>
+#include <linux/fsl_devices.h>
+#include <linux/spi/spi.h>
+#include <linux/i2c.h>
+#include <linux/mtd/mtd.h>
+#include <linux/mtd/map.h>
+#include <linux/mtd/partitions.h>
+#include <linux/regulator/consumer.h>
+#include <linux/pmic_external.h>
+#include <linux/pmic_status.h>
+#include <linux/ipu.h>
+#include <linux/mxcfb.h>
+#include <linux/pwm_backlight.h>
+#include <mach/common.h>
+#include <mach/hardware.h>
+#include <asm/setup.h>
+#include <asm/mach-types.h>
+#include <asm/mach/arch.h>
+#include <asm/mach/time.h>
+#include <asm/mach/keypad.h>
+#include <asm/mach/flash.h>
+#include <mach/gpio.h>
+#include <mach/mmc.h>
+#include <mach/mxc_dvfs.h>
+#include <mach/mxc_edid.h>
+#include <mach/iomux-mx51.h>
+#include <mach/i2c.h>
+
+#include "devices.h"
+#include "crm_regs.h"
+#include "usb.h"
+#include "mx51_pins_my.h"
+#endif
+
+
+
+#define PROCFS_MAX_SIZE         1024
+#define PROCFS_NAME             "gpio8308"
+static int ledon=0;
+static int goffset=0x00000c08;
+static int gwoffset=0x00000c08;
+static int gv=0;
+#define BABBAGE_DVI_RESET		(2*32 + 5)	/* GPIO_3_5 */
+
+/**
+ * This structure hold information about the /proc file
+ *
+ */
+static struct proc_dir_entry *Our_Proc_File;
+
+/**
+ * The buffer used to store character for this module
+ *
+ */
+static char procfs_buffer[PROCFS_MAX_SIZE];
+static char ascii_tbl[]="0123456789ABCDEF";
+
+/**
+ * The size of the buffer
+ *
+ */
+static unsigned long procfs_buffer_size = 0;
+
+static int setl_reg(int addr,int v)
+{
+	void __iomem *immap;
+	immap = ioremap(get_immrbase(), 0x1000);
+	if (immap) {
+		out_be32(immap + addr, v);
+		iounmap(immap);
+	} 
+	return 0;
+}
+
+long int get_gpio6(void)
+{
+//#define GP1DAT_OFFSET	0x00000c08
+#define GP1DAT_MASK		0xfc000000
+	__be32 __iomem *spcr = ioremap(get_immrbase() + goffset, 4);
+	__be32 tmp;
+
+	tmp = in_be32(spcr);
+	//out_be32(spcr, tmp | SPCR_TBEN);
+
+	iounmap(spcr);
+
+	return tmp;
+}
+
+
+/** 
+ * This function is called then the /proc file is read
+ *
+ */
+int 
+procfile_read(char *buffer,
+              char **buffer_location,
+              off_t offset, int buffer_length, int *eof, void *data)
+{
+        int ret;
+        int v;
+        
+        //printk(KERN_INFO " == procfile_read (/proc/%s) called offset:0x%08x \n", PROCFS_NAME,offset);
+		v=get_gpio6();
+        
+        if (offset > 0) {
+        //printk(KERN_INFO "  1 procfile_read (/proc/%s) called\n", PROCFS_NAME);
+                /* we have finished to read, return 0 */
+		*eof=1;////
+                ret  = 0;
+        } else {
+        //printk(KERN_INFO "  0 procfile_read (/proc/%s) called\n", PROCFS_NAME);
+                /* fill the buffer, return the buffer size */
+		sprintf(procfs_buffer,"%08x\n",v);
+		//procfs_buffer[9]=0;
+
+                memcpy(buffer, procfs_buffer, 9/*procfs_buffer_size*/);
+                ret = 9/*procfs_buffer_size*/;
+        }
+
+        return ret;
+}
+
+/**
+ * This function is called with the /proc file is written
+ *
+ */
+int procfile_write(struct file *file, const char *buffer, unsigned long count,
+                   void *data)
+{
+	int n;
+        /* get buffer size */
+        procfs_buffer_size = count;
+        if (procfs_buffer_size > PROCFS_MAX_SIZE ) {
+                procfs_buffer_size = PROCFS_MAX_SIZE;
+        }
+        //procfs_buffer_size = 1;
+        
+        /* write data to the buffer */
+        if ( copy_from_user(procfs_buffer, buffer, procfs_buffer_size ) ) {
+                return -EFAULT;
+        }
+        procfs_buffer[procfs_buffer_size]=0;
+        switch( 0x0ff & procfs_buffer[0] ){
+          case 'a':
+            sscanf(procfs_buffer+1,"%x",&goffset);
+            printk(" gpio addr : 0x%x\n",goffset);
+            break;
+          default:
+            n=sscanf(procfs_buffer+1,"%x%x",&gwoffset,&gv);
+            if(n!=2)break;
+            setl_reg(gwoffset,gv);
+            break; 
+	}
+
+		//if( procfs_buffer[0] == '0' ) ledon=0;
+		//else ledon=1;
+		//procfs_buffer[1]=0x0a;
+		//procfs_buffer[2]=0;
+        //printk(KERN_INFO "procfile_write (/proc/%s) called 0x%02x count:%d\n", PROCFS_NAME, 0x0ff & procfs_buffer[0],count);
+
+		//gpio_set_value(BABBAGE_DVI_RESET, ledon);
+		//ledon = (ledon+1)&1;
+        
+        return procfs_buffer_size;
+}
+
+/**
+ *This function is called when the module is loaded
+ *
+ */
+int init_module()
+{
+        /* create the /proc file */
+        Our_Proc_File = create_proc_entry(PROCFS_NAME, 0644, NULL);
+        
+        if (Our_Proc_File == NULL) {
+                remove_proc_entry(PROCFS_NAME, NULL);
+                printk(KERN_ALERT "Error: Could not initialize /proc/%s\n",
+                        PROCFS_NAME);
+                return -ENOMEM;
+        }
+
+        Our_Proc_File->read_proc  = procfile_read;
+        Our_Proc_File->write_proc = procfile_write;
+        //Our_Proc_File->owner           = THIS_MODULE;
+        Our_Proc_File->mode    = S_IFREG | S_IRUGO;
+        Our_Proc_File->uid     = 0;
+        Our_Proc_File->gid     = 0;
+        Our_Proc_File->size    = 37;
+
+        printk(KERN_INFO "/proc/%s created\n", PROCFS_NAME);    
+        return 0;       /* everything is ok */
+}
+
+/**
+ *This function is called when the module is unloaded
+ *
+ */
+void cleanup_module()
+{
+        remove_proc_entry(PROCFS_NAME, NULL);
+        printk(KERN_INFO "/proc/%s removed\n", PROCFS_NAME);
+}
+
+module_init(init_module);
+module_exit(cleanup_module);
+
+
+
--- mpc8308/k.cfm/readme.txt
+++ mpc8308/k.cfm/readme.txt
@@ -1,10 +1,10 @@
 kernel cfm
+4.5 kernel ver c3f: add gpio proc driver
 4. kernel ver c3a: reboot bug fix
 4.1 kernel ver c3b: reboot bug fix, add udelay (setup-common.c
 4.2 kernel ver c3c: reboot bug fix, udelay ==> printk 
 4.3 kernel ver c3c: reboot bug fix, printk ==> udelay
 4.4 kernel ver c3c: reboot bug fix, add udelay after ..shutdown() (
-setup-common.c 
 3. c1: add config uinput
 2. kernel bin : cuImage 
 1. dir kernel cfm

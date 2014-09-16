c5cace537b76b3779786dbc60907636e17debc3a
guguke <gbm618@gmail.com>
2014-09-16 15:07:06 -0400

    mk

--- p1020/cfm/u/mk.sh
+++ p1020/cfm/u/mk.sh
@@ -5,10 +5,13 @@
 #echo "CROSS_COMPILE=ppc_85xx-"
 #export CROSS_COMPILE=ppc_85xx-
 
-echo "export PATH=$PATH:/opt/eldk-5.4/powerpc-e500v2/sysroots/i686-eldk-linux/usr/bin/ppce500v2-linux-gnuspe/"
-export PATH=$PATH:/opt/eldk-5.4/powerpc-e500v2/sysroots/i686-eldk-linux/usr/bin/ppce500v2-linux-gnuspe/
-echo "CROSS_COMPILE=powerpc-e500v2-"
-export CROSS_COMPILE=powerpc-e500v2-
+#echo "export PATH=$PATH:/opt/eldk-5.4/powerpc-e500v2/sysroots/i686-eldk-linux/usr/bin/ppce500v2-linux-gnuspe/"
+#export PATH=$PATH:/opt/eldk-5.4/powerpc-e500v2/sysroots/i686-eldk-linux/usr/bin/ppce500v2-linux-gnuspe/
+#echo "CROSS_COMPILE=powerpc-e500v2-"
+#export CROSS_COMPILE=powerpc-e500v2-
+
+export PATH=$PATH:/opt/freescale/usr/local/gcc-4.3.74-eglibc-2.8.74-dp-2/powerpc-none-linux-gnuspe/bin/
+export CROSS_COMPILE=powerpc-none-linux-gnuspe-
 
 make distclean
 #make P1020RDB_NAND_config

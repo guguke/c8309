7961edc50fa2743c4ffd126f1c3575685d50248a
guguke <gbm618@gmail.com>
2015-07-22 18:18:10 +0800

    read gpio8308

--- broadcast/linux/readgpio8308.c
+++ broadcast/linux/readgpio8308.c
@@ -12,26 +12,51 @@
 #include <sys/reboot.h>
 #include <pthread.h>
 
+int wvgpio(int addr,int v)
+{
+	FILE *fp;
+	int v;
+	fp = fopen("/proc/gpio8308","rt");
+	fprintf(fp,"a%08x %0x8",addr,v);
+	fclose(fp);
+	return 0;
+}
+int wagpio(int addr)
+{
+	FILE *fp;
+	int v;
+	fp = fopen("/proc/gpio8308","rt");
+	fprintf(fp,"a%08x",addr);
+	fclose(fp);
+	return 0;
+}
+
+int readgpio()
+{
+	FILE *fp;
+	int v;
+	fp = fopen("/proc/gpio8308","rt");
+	fscanf(fp,"%x",&v);
+	fclose(fp);
+	return v;
+}
+
 int main(int argc,char *argv[])
 {
 	int v;
 	int n=0;
 	int k1=0x800,k2=0x800,k3=0x800;
-	FILE *fp;
-	fp=fopen("/proc/gpio8308","w+t");
-	if(fp==NULL) return -1;
-	fprintf(fp,"a0118");
-	usleep(100000);
-	fscanf(fp,"%x",&v);
-	usleep(100000);
-	fprintf(fp,"a0118 %08x",v|0xfc000000);
-	usleep(100000);
-	fprintf(fp,"a0c08");
-	fscanf(fp,"%x",&v);
+
+	wagpio(0x0118);
+	v = readgpio();
+	wvgpio(0x0118,v|0xfc000000);
+	wagpio(0x0c08);
+
+	v = readgpio();
 	printf(" 1 read addr(0x0c08): %08x\n",v);
 	usleep(100000);
 	for(;;){
-		fscanf(fp,"%x",&v);
+		v = readgpio();
 		if((0x800&v)!=k1){
 			printf(" read addr(0x0118) : %08x\n",v);
 			k1 = v & 0x800;
@@ -43,7 +68,6 @@ int main(int argc,char *argv[])
 
 	//usleep(1000000);// 1s
 
-	fclose(fp);
 	return 0;
 }
 

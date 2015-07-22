32b6463354c165875a7fb4040e13cbcf8cf3a15b
guguke <gbm618@gmail.com>
2015-07-22 17:52:15 +0800

    read gpio8308

--- broadcast/linux/readgpio8308.c
+++ broadcast/linux/readgpio8308.c
@@ -14,12 +14,27 @@
 
 int main(int argc,char *argv[])
 {
+	int v;
+	int n=0;
+	int k1=0x800,k2=0x800,k3=0x800;
 	FILE *fp;
 	fp=fopen("/proc/gpio8308","w+t");
 	if(fp==NULL) return -1;
 	fprintf(fp,"a0118");
 	fscanf(fp,"%x",&v);
-	printf(" read addr(0x0118) : 0x%08x\n",v);
+	fprintf(fp,"a0118 %08x",v|0xfc000000);
+	for(;;){
+		fscanf(fp"%x"&v);
+		if((0x800&v)!=k1){
+			printf(" read addr(0x0118) : %08x\n",v);
+			k1 = v & 0x800;
+			n++;
+			if(n>5)break;
+		}
+		usleep(100000);
+	}
+
+	//usleep(1000000);// 1s
 
 	fclose(fp);
 	return 0;
